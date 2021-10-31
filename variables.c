7978faa17d65267ccbe66abc6ee175cb77e779b3
Ilheme DERDOUR <35100633+ilhemederdour@users.noreply.github.com>
2021-10-31 21:38:49 +0100

    Update variables.c

---
+++ variables.c
@@ -0,0 +1,22 @@
+//on va voir c'est quoi une variable et son utilité.
+
+#include<stdio.h>
+int main(){
+	// toute variable doit etre déclarée
+	//tout variable a un type
+	int a;
+	int b;
+	int c;
+	//une variable peut avoir une valeur initiale (non obligé)
+	char e = 'H';
+	a= 12;
+	b= 13;
+	c= a+b;
+	
+	printf("La somme de %d et %d est : %d ",a,b,c);
+	
+	
+	
+	
+	return 0; 
+}
